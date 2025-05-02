55c5cd781e47f03b059aead087d8b08ca9c79e57
k.seretis <serkleanthis@gmail.com>
2025-05-02 13:05:23 +0300

    Unit test update

--- Tests/DataTypes/OverrideDataTypeTest.cs
+++ Tests/DataTypes/OverrideDataTypeTest.cs
@@ -64,7 +64,6 @@ public class OverrideDataTypeTest
     
     [TestMethod]
     [DataRow("Croc")]
-    [DataRow("Crod")]
     public void CreateName_WithOverrideValidation_ShouldThrow(string value)
     {
         Name.OverrideDefaultValidation = ShouldBeMoreThan5Characters;
